9f739c7eb5250eae29f738503471ce63cc4ce31c
decembaek <tmdrbpp123@gmail.com>
2023-08-04 23:17:44 +0900

    깃은 습관화 시키자..
    일단 공부하던거 views.py 백업시켜둠

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,7 +5,13 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+
+        #보여줄 필드    __all__ 할시 전부 공개
         fields = (
             "name",
             "kind",
         )
+        # exclude 제외시키기
+        # exclude = (
+        #     "create_dat"
+        # )
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views_1044
 
 urlpatterns = [
     path("", views.Rooms.as_view()),
