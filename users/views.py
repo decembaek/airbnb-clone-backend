1d8563997ebb0fbf7747bea8b95a28e45e74b284
decembaek <tmdrbpp123@gmail.com>
2023-08-24 21:43:46 +0900

    패스워드 바꾸기

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated  # 권한
+
 from . import serializers
+from .models import User
 
 
 class Me(APIView):
@@ -26,3 +29,55 @@ class Me(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # set_password Django 패스워드 만들기 HASH 해쉬로 만들기
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        # 추후에 Public으로 바꾸기
+        # 1. 사용자들이 내가 남긴 리뷰들을 볼수있게
+        # 2. 내가 얼마나 집을 가지고 있는지
+        # 3. 내가 어떤 도시들을 여행했는지
+        # 4. 내게 남긴 리뷰들을 볼 수 있게 만들기
+        except User.DoesNotExist:
+            raise NotFound
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+# PUT
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        # Django Hash 해쉬 비밀번호 확인하기 True면 일치
+        if user.check_password(old_password):
+            # Hash로 비번 저장
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
